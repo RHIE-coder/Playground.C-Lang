e564243c3ed7a18429cb739bc7ba61b58a5b9b5b
MinHyungRHIE <quotia72@naver.com>
2021-04-10 21:29:47 +0900

    수정

--- notebook.c
+++ notebook.c
@@ -1,11 +1,36 @@
 #include <stdio.h>
 
-int main(int argc, char *argv[])
+int add(int, int);
+
+int mul(int, int);
+
+int calculator(int, int, int (*func)(int, int));
+
+int main()
 {
-    printf("argc : %d\n", argc);
-    for (int i = 0; i < argc; i++)
-    {
-        printf("%s\n", argv[i]);
-    }
+    int (*add_calc)(int, int) = add;
+    printf("add_calc : %d\n", add_calc(10, 20));
+    int (*mul_calc)(int, int) = mul;
+    printf("mul_calc : %d\n", mul_calc(10, 20));
+    int add_result = calculator(10, 20, add);
+    int mul_result = calculator(10, 20, mul);
+    printf("add : %d\n", add_result);
+    printf("mul : %d\n", mul_result);
+
     return 0;
+}
+
+int add(int num1, int num2)
+{
+    return num1 + num2;
+}
+
+int mul(int num1, int num2)
+{
+    return num1 * num2;
+}
+
+int calculator(int num1, int num2, int (*func)(int, int))
+{
+    return func(num1, num2);
 }
