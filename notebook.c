06af578af614975d061af1d6a3e08d79d383c4ac
rhie-coder <quotia72@naver.com>
2021-05-04 23:45:27 +0900

    fopen

--- notebook.c
+++ notebook.c
@@ -1,29 +1,23 @@
 #include <stdio.h>
 
-enum Services
-{
-    LOGIN, REGISTER, ORDER=5
-};//0, 1, 2
+void readFile(FILE * file){
+    char ch;
+    while((ch=fgetc(file))!=NULL){
+        putchar(ch);
+    }
+}
 
 int main()
 {
-    int sel = 1;
-
-    switch(sel){
-        case LOGIN:
-            puts("로그인 서비스");
-            break;
-
-        case REGISTER:
-            puts("회원가입 서비스");
-            break;
+    FILE * file = fopen("sample.txt", "r");
+    if(file != NULL){
+        puts("sample.txt 열기 성공");
         
-        case ORDER:
-            puts("주문 서비스");
-            break;
-    }//회원가입 서비스
+    }
 
-    printf("%d %d %d\n", LOGIN, REGISTER, ORDER); //0 1 5
-
+    if(fclose(file) != EOF){ //fclose(file) == 0
+        puts("sample.txt 닫기 성공");
+    }
+    
     return 0;
 }
